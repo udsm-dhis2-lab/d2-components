7e9d13da1e8258d9b83ac00573ef76b71f258423
Rajab Mkomwa <rajeyw@gmail.com>
2025-05-01 10:53:19 +0300

    refactor: update approach to initialize web sdk in shell

--- packages/ng-dhis2-shell/src/lib/ng-dhis2-shell.module.ts
+++ packages/ng-dhis2-shell/src/lib/ng-dhis2-shell.module.ts
@@ -1,55 +1,27 @@
 import {
-  HttpClient,
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import {
-  InjectionToken,
   ModuleWithProviders,
   NgModule,
-  inject,
   provideAppInitializer,
 } from '@angular/core';
-import {
-  AppShellConfig,
-  AppShellConfigClass,
-  AppShellConfigService,
-} from './models';
+import { D2Web } from '@iapps/d2-web-sdk';
+import { AppShellConfig, AppShellConfigClass } from './models';
 import { NgDhis2ShellComponent } from './ng-dhis2-shell.component';
-import { firstValueFrom } from 'rxjs';
-import { D2Web, D2WebConfig } from '@iapps/d2-web-sdk';
 
-function initializeShell(
-  appShellConfig: AppShellConfigClass,
-  httpClient: HttpClient
-) {
-  const configService = new AppShellConfigService(appShellConfig, httpClient);
-
+function initializeShell(): () => Promise<void> {
   return async () => {
-    await initializeWedSDK(configService);
-    return configService;
+    try {
+      await D2Web.initialize({});
+    } catch (e) {
+      console.warn('Web SDK could not get initialized');
+    }
+
+    return Promise.resolve();
   };
 }
-
-async function initializeWedSDK(
-  appShellConfig: AppShellConfigService
-): Promise<void> {
-  try {
-    const config = await firstValueFrom(appShellConfig.getConfig());
-
-    if (window) {
-      (window as any).d2Web = await D2Web.initialize(
-        new D2WebConfig({
-          baseUrl: config.url,
-        })
-      );
-    }
-  } catch (e) {
-    console.warn('WebSDK could not get initialized');
-  }
-}
-
-export const D2_WEB_SDK = new InjectionToken<any>('D2WebSDK');
 
 @NgModule({
   declarations: [NgDhis2ShellComponent],
@@ -64,10 +36,7 @@ export class AppShellModule {
       providers: [
         { provide: AppShellConfigClass, useValue: config },
         provideAppInitializer(() => {
-          return initializeShell(
-            inject(AppShellConfigClass),
-            inject(HttpClient)
-          )() as unknown as Promise<void>;
+          return initializeShell()() as unknown as Promise<void>;
         }),
       ],
     };
