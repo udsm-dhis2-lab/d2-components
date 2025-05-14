a1c1adf8501ce16e89e0dc4394202ff84ef897ff
formayhem <nsaji6561@gmail.com>
2025-05-14 09:36:13 +0300

    feat: improve filters layout

--- packages/ng-dhis2-ui/src/lib/modules/line-list/components/table/filterToolbar.tsx
+++ packages/ng-dhis2-ui/src/lib/modules/line-list/components/table/filterToolbar.tsx
@@ -45,7 +45,7 @@ export const FilterToolbar = ({
     <div
       style={{
         display: 'flex',
-        flexDirection: 'column',
+        // flexDirection: 'column',
         gap: '10px',
         flexWrap: 'wrap',
       }}
@@ -117,14 +117,14 @@ export const FilterToolbar = ({
           </>
         )}
 
-        <Button onClick={handleSearch}>Search</Button>
+        {/* <Button onClick={handleSearch}>Search</Button>
         <Button onClick={() => setShowAllFilters(!showAllFilters)}>
           {showAllFilters ? 'Less Filters' : 'More Filters'}
-        </Button>
+        </Button> */}
       </div>
 
       {/* Row 2: visibleFilters appear in a new row */}
-      {visibleFilters?.length > 0 && (
+      {/* {visibleFilters?.length > 0 && (
         <div
           style={{
             display: 'flex',
@@ -132,263 +132,113 @@ export const FilterToolbar = ({
             gap: '10px',
             flexWrap: 'wrap',
           }}
-        >
-          {(visibleFilters ?? []).map(
-            ({ label, key, valueType, options }: any) => {
-              if (valueType === 'DATE') {
-                return (
-                  <CalendarInput
-                    key={key}
-                    label={`${label}:`}
-                    calendar="gregory"
-                    locale="en-GB"
-                    timeZone="Africa/Dar_es_Salaam"
-                    className="custom-input"
-                    clearable={!!dateStates[key]}
-                    date={dateStates[key]}
-                    onDateSelect={(selectedDate: any) => {
-                      handleDateSelect(key, selectedDate);
-                      const filteredFilters = dataQueryFiltersState.filter(
-                        (f: { attribute: any }) => f.attribute !== key
-                      );
-                      if (selectedDate.calendarDateString === null) {
-                        console.log('selectedDate', selectedDate);
-                        setDataQueryFiltersState(filteredFilters);
-                      }
-                    }}
-                  />
+        > */}
+      {(visibleFilters ?? []).map(({ label, key, valueType, options }: any) => {
+        if (valueType === 'DATE') {
+          return (
+            <CalendarInput
+              key={key}
+              label={`${label}:`}
+              calendar="gregory"
+              locale="en-GB"
+              timeZone="Africa/Dar_es_Salaam"
+              className="custom-input"
+              clearable={!!dateStates[key]}
+              date={dateStates[key]}
+              onDateSelect={(selectedDate: any) => {
+                handleDateSelect(key, selectedDate);
+                const filteredFilters = dataQueryFiltersState.filter(
+                  (f: { attribute: any }) => f.attribute !== key
                 );
-              }
+                if (selectedDate.calendarDateString === null) {
+                  console.log('selectedDate', selectedDate);
+                  setDataQueryFiltersState(filteredFilters);
+                }
+              }}
+            />
+          );
+        }
 
-              if (options && options.options.length > 0) {
-                return (
-                  <SingleSelectField
-                    key={key}
-                    label={`${label}:`}
-                    selected={inputValues[key] || ''}
-                    className="custom-select-input"
-                    clearable
-                    onChange={({ selected }: { selected: string }) => {
-                      setInputValues((prevValues: any) => ({
-                        ...prevValues,
-                        [key]: selected ?? '',
-                      }));
-                      handleInputChangeForSelectField(key, selected ?? '');
-                      const filteredFilters = dataQueryFiltersState.filter(
-                        (f: { attribute: any }) => f.attribute !== key
-                      );
-                      if (selected === '') {
-                        setDataQueryFiltersState(filteredFilters);
-                      }
-                    }}
-                  >
-                    {(options.options ?? []).map((opt: any) => (
-                      <SingleSelectOption
-                        key={opt.id}
-                        label={opt.name}
-                        value={opt.code}
-                      />
-                    ))}
-                  </SingleSelectField>
+        if (options && options.options.length > 0) {
+          return (
+            <SingleSelectField
+              key={key}
+              label={`${label}:`}
+              selected={inputValues[key] || ''}
+              className="custom-select-input"
+              clearable
+              onChange={({ selected }: { selected: string }) => {
+                setInputValues((prevValues: any) => ({
+                  ...prevValues,
+                  [key]: selected ?? '',
+                }));
+                handleInputChangeForSelectField(key, selected ?? '');
+                const filteredFilters = dataQueryFiltersState.filter(
+                  (f: { attribute: any }) => f.attribute !== key
                 );
-              }
-
-              return (
-                <InputField
-                  key={key}
-                  label={`${label}:`}
-                  className="custom-input"
-                  value={inputValues[key] || ''}
-                  clearable
-                  onChange={(e: any) => {
-                    const currentValue =
-                      'target' in e ? e.target.value : e.value ?? '';
-                    setInputValues((prevValues: any) => ({
-                      ...prevValues,
-                      [key]: currentValue,
-                    }));
-                    const filteredFilters = dataQueryFiltersState.filter(
-                      (f: { attribute: any }) => f.attribute !== key
-                    );
-                    if (currentValue === '') {
-                      setDataQueryFiltersState(filteredFilters);
-                    }
-                  }}
-                  onBlur={(e: any) => {
-                    const currentValue =
-                      'target' in e ? e.target.value : e.value ?? '';
-                    if (currentValue !== '' && currentValue !== prevValue) {
-                      handleInputChange(key, currentValue);
-                      setPrevValue(currentValue);
-                    }
-                  }}
+                if (selected === '') {
+                  setDataQueryFiltersState(filteredFilters);
+                }
+              }}
+            >
+              {(options.options ?? []).map((opt: any) => (
+                <SingleSelectOption
+                  key={opt.id}
+                  label={opt.name}
+                  value={opt.code}
                 />
+              ))}
+            </SingleSelectField>
+          );
+        }
+
+        return (
+          <InputField
+            key={key}
+            label={`${label}:`}
+            className="custom-input"
+            value={inputValues[key] || ''}
+            clearable
+            onChange={(e: any) => {
+              const currentValue =
+                'target' in e ? e.target.value : e.value ?? '';
+              setInputValues((prevValues: any) => ({
+                ...prevValues,
+                [key]: currentValue,
+              }));
+              const filteredFilters = dataQueryFiltersState.filter(
+                (f: { attribute: any }) => f.attribute !== key
               );
-            }
-          )}
-        </div>
-      )}
+              if (currentValue === '') {
+                setDataQueryFiltersState(filteredFilters);
+              }
+            }}
+            onBlur={(e: any) => {
+              const currentValue =
+                'target' in e ? e.target.value : e.value ?? '';
+              if (currentValue !== '' && currentValue !== prevValue) {
+                handleInputChange(key, currentValue);
+                setPrevValue(currentValue);
+              }
+            }}
+          />
+        );
+      })}
+      {/* </div>
+      )} */}
+      <div
+        style={{
+          display: 'flex',
+          // alignItems: 'center',
+          gap: '10px',
+          // flexWrap: 'wrap',
+        }}
+      >
+        <Button onClick={handleSearch}>Search</Button>
+        <Button onClick={() => setShowAllFilters(!showAllFilters)}>
+          {showAllFilters ? 'Less Filters' : 'More Filters'}
+        </Button>
+      </div>
     </div>
   </DataTableToolbar>
-
-  // <DataTableToolbar className="table-top-toolbar">
-  //   <div
-  //     style={{
-  //       display: 'flex',
-  //       alignItems: 'center',
-  //       gap: '10px',
-  //       flexWrap: 'wrap',
-  //     }}
-  //   >
-  //     <InputField
-  //       key="location"
-  //       label={orgUnitLabel ? orgUnitLabel : 'registering unit:'}
-  //       value={selectedOrgUnit}
-  //       onFocus={() => setHide(false)}
-  //       className="custom-input"
-  //       clearable
-  //       onChange={(event: { value: string }) => {
-  //         setSelectedOrgUnit(event.value);
-  //         setOrgUnitState(defaultOrgUnit);
-  //       }}
-  //     />
-  //     <CalendarInput
-  //       label="Start date:"
-  //       calendar="gregory"
-  //       locale="en-GB"
-  //       timeZone="Africa/Dar_es_Salaam"
-  //       className="custom-input"
-  //       clearable={!!tempStartDateState}
-  //       date={tempStartDateState}
-  //       onDateSelect={(selectedDate: any) => {
-  //         // if (!(selectedDate.calendarDateString === null)) {
-  //         setTempStartDateState(selectedDate.calendarDateString);
-  //         //}
-  //         if (selectedDate.calendarDateString === null) {
-  //           setStartDateState(selectedDate.calendarDateString);
-  //         }
-  //       }}
-  //     />
-  //     <CalendarInput
-  //       label="End date:"
-  //       calendar="gregory"
-  //       locale="en-GB"
-  //       timeZone="Africa/Dar_es_Salaam"
-  //       className="custom-input"
-  //       clearable={!!tempEndDateState}
-  //       date={tempEndDateState}
-  //       onDateSelect={(selectedDate: any) => {
-  //         // if (!(selectedDate.calendarDateString === null)) {
-  //         setTempEndDateState(selectedDate.calendarDateString);
-  //         //}
-  //         if (selectedDate.calendarDateString === null) {
-  //           setEndDateState(selectedDate.calendarDateString);
-  //         }
-  //       }}
-  //     />
-  //     <Button onClick={() => handleSearch()}>Search</Button>
-  //     <Button onClick={() => setShowAllFilters(!showAllFilters)}>
-  //       {showAllFilters ? 'Less Filters' : 'More Filters'}
-  //     </Button>
-
-  //     {(visibleFilters ?? []).map(({ label, key, valueType, options }: any) => {
-  //       if (valueType === 'DATE') {
-  //         return (
-  //           <CalendarInput
-  //             key={key}
-  //             label={`${label}:`}
-  //             calendar="gregory"
-  //             locale="en-GB"
-  //             timeZone="Africa/Dar_es_Salaam"
-  //             className="custom-input"
-  //             clearable={!!dateStates[key]}
-  //             date={dateStates[key]}
-  //             onDateSelect={(selectedDate: any) => {
-  //               handleDateSelect(key, selectedDate);
-  //               const filteredFilters = dataQueryFiltersState.filter(
-  //                 (f: { attribute: any }) => f.attribute !== key
-  //               );
-
-  //               if (selectedDate === null) {
-  //                 setDataQueryFiltersState(filteredFilters);
-  //               }
-  //             }}
-  //           />
-  //         );
-  //       }
-  //       // const filteredFilters = prevFilters.filter((f) => f.attribute !== key);
-  //       if (options && options.options.length > 0) {
-  //         return (
-  //           <SingleSelectField
-  //             key={key}
-  //             label={`${label}:`}
-  //             selected={inputValues[key] || ''}
-  //             className="custom-select-input"
-  //             clearable
-  //             onChange={({ selected }: { selected: string }) => {
-  //               setInputValues((prevValues: any) => ({
-  //                 ...prevValues,
-  //                 [key]: selected ?? '',
-  //               }));
-  //               handleInputChangeForSelectField(key, selected ?? '');
-  //               const filteredFilters = dataQueryFiltersState.filter(
-  //                 (f: { attribute: any }) => f.attribute !== key
-  //               );
-  //               if (selected === '') {
-  //                 setDataQueryFiltersState(filteredFilters);
-  //               }
-  //             }}
-  //           >
-  //             {(options.options ?? []).map((opt: any) => (
-  //               <SingleSelectOption
-  //                 key={opt.id}
-  //                 label={opt.name}
-  //                 value={opt.code}
-  //               />
-  //             ))}
-  //           </SingleSelectField>
-  //         );
-  //       }
-
-  //       return (
-  //         <InputField
-  //           key={key}
-  //           label={`${label}:`}
-  //           className="custom-input"
-  //           value={inputValues[key] || ''}
-  //           clearable
-  //           onChange={(e: any) => {
-  //             const currentValue =
-  //               'target' in e ? e.target.value : e.value ?? '';
-  //             setInputValues((prevValues: any) => ({
-  //               ...prevValues,
-  //               [key]: currentValue,
-  //             }));
-  //             // if (currentValue === '') {
-  //             //   handleInputChange(key, '');
-  //             // }
-  //             const filteredFilters = dataQueryFiltersState.filter(
-  //               (f: { attribute: any }) => f.attribute !== key
-  //             );
-
-  //             if (currentValue === '') {
-  //               setDataQueryFiltersState(filteredFilters);
-  //             }
-  //           }}
-  //           onBlur={(e: any) => {
-  //             const currentValue =
-  //               'target' in e ? e.target.value : e.value ?? '';
-  //             if (currentValue !== '' && currentValue !== prevValue) {
-  //               handleInputChange(key, currentValue);
-  //               setPrevValue(currentValue);
-  //             }
-  //           }}
-  //         />
-  //       );
-  //     })}
-  //     <div>
-  //       {/* <p>hello</p> */}
-  //     </div>
-  //   </div>
-  // </DataTableToolbar>
 );
