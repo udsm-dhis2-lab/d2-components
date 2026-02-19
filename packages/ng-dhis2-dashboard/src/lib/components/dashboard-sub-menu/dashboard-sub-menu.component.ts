ef572b95e522337a89ee5cbf2c18db5f271246ab
Walter V. Faustine <waltervfaustine@gmail.com>
2026-02-19 17:30:20 +0300

    feat: improve dashboard sub-menus

--- packages/ng-dhis2-dashboard/src/lib/components/dashboard-sub-menu/dashboard-sub-menu.component.ts
+++ packages/ng-dhis2-dashboard/src/lib/components/dashboard-sub-menu/dashboard-sub-menu.component.ts
@@ -1,13 +1,16 @@
+/* eslint-disable @angular-eslint/component-selector */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DashboardMenuObject } from '../../models';
 import { CommonModule } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
-    imports: [CommonModule, MatTooltipModule],
-    selector: 'd2-dashboard-sub-menu',
-    templateUrl: './dashboard-sub-menu.component.html',
-    styleUrls: ['./dashboard-sub-menu.component.scss']
+  imports: [CommonModule, MatTooltipModule],
+  selector: 'd2-dashboard-sub-menu',
+  templateUrl: './dashboard-sub-menu.component.html',
+  styleUrls: ['./dashboard-sub-menu.component.scss'],
 })
 export class DashboardSubMenuComponent implements OnInit {
   @Input() dashboardSubMenus!: DashboardMenuObject[];
@@ -26,4 +29,6 @@ export class DashboardSubMenuComponent implements OnInit {
     event.stopPropagation();
     this.setCurrentDashboardSubMenu.emit(dashboardSubMenu);
   }
+
+  trackById = (_: number, item: any) => item?.id;
 }
